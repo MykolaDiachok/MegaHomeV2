6c10aa6b27129e43eb294a39b01fa5b335662599
Дячок Микола Володимирович <m.diachok@kt.ua>
2018-08-05 21:24:30 +0300

    test

--- Ventilator.cpp
+++ Ventilator.cpp
@@ -14,7 +14,7 @@ Ventilator::Ventilator() {}
 
 void Ventilator::on()
 {
-    //TODO включить когда сделают разводку
+    //TODO включить когда сделают разводку1
     //digitalWrite(_pin, HIGH); 
     Serial.print("ventilator=on; pin=");
     Serial.println(_pin);
